5aa167eb38f8013c91785c39834d1398728fd817
Rob Jinman <admin@robjinman.com>
2016-08-05 15:56:05 +0100

    Update dist

--- dist/jloop-compiled.js
+++ dist/jloop-compiled.js
@@ -22,16 +22,17 @@ module.exports = {
 };
 
 
-},{"./utils":7}],3:[function(require,module,exports){
+},{"./utils":8}],3:[function(require,module,exports){
 window.jloop = require("./jloop");
 
 
 },{"./jloop":4}],4:[function(require,module,exports){
 var config = require("./config");
 var model = require("./model");
-var session = require("./session");
+var sessions = require("./session");
 var utils = require("./utils");
 var err = require("./exceptions");
+var syncedTranscript = require("./syncedTranscript");
 
 /**
  * @class JLoopChat
@@ -41,46 +42,6 @@ var jLoopChat = function(spec, my) {
   //
   var _fnOnAgentMessage = null;
   var _fnOnAgentStatusChange = null;
-
-  function _addToTranscript(e) {
-    var transcript = session.get("transcript", session.Transcript);
-    transcript.addEvent(e);
-  }
-
-  function _checkInitialised() {
-    if (my.initialised === false) {
-      throw new err.JLoopException("jLoopChat not initialised");
-    }
-  }
-
-  function _onAgentMessage(e) {
-    if (_fnOnAgentMessage) {
-      _fnOnAgentMessage(e);
-    }
-  }
-
-  function _onAgentStatusChange(e) {
-    if (_fnOnAgentStatusChange) {
-      _fnOnAgentStatusChange(e);
-    }
-  }
-
-  function _onMessage(m) {
-    var e = JSON.parse(m.data);
-    console.log(e);
-
-    _addToTranscript(e);
-
-    if (e.eventType == "AgentMessage") {
-      _onAgentMessage(e);
-    }
-    else if (e.eventType == "AgentStatusChange") {
-      _onAgentStatusChange(e);
-    }
-    else {
-      throw new err.JLoopException("Unknown event type '" + eventType + "'");
-    }
-  }
 
   // Protected
   //
@@ -93,11 +54,9 @@ var jLoopChat = function(spec, my) {
   //
   var that = {};
   that.customerId = spec.customerId;
-  that.visitorId = null;
-
-  that.getTranscript = function() {
-    return session.get("transcript", session.Transcript);
-  };
+  that.visitorId = utils.generateUuid();
+  that.session = sessions.session(spec.uniquePrefix || "jl");
+  that.transcript = syncedTranscript(that.session);
 
   that.setOnAgentMessage = function(fn) {
     _fnOnAgentMessage = fn;
@@ -113,7 +72,7 @@ var jLoopChat = function(spec, my) {
   * @param {Function} fnFailure (Optional) A no argument function
   */
   that.initialise = function(fnSuccess, fnFailure) {
-    that.visitorId = session.get("visitorId");
+    that.visitorId = that.session.get("visitorId");
 
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://" + config.serverLookupBaseUri + "/endpoint?cid=" + that.customerId, true);
@@ -168,7 +127,7 @@ var jLoopChat = function(spec, my) {
   that.sendMessage = function(msg) {
     _checkInitialised();
 
-    _addToTranscript(msg);
+    that.transcript.addEvent(msg);
 
     console.log("Sending...");
     console.log(msg);
@@ -191,7 +150,7 @@ var jLoopChat = function(spec, my) {
       status: "online"
     });
 
-    _addToTranscript(event);
+    that.transcript.addEvent(event);
 
     console.log("Sending..."); // TODO
     console.log(event);
@@ -215,7 +174,7 @@ var jLoopChat = function(spec, my) {
       status: "offline"
     });
 
-    _addToTranscript(event);
+    that.transcript.addEvent(event);
 
     console.log("Sending..."); // TODO
     console.log(event);
@@ -226,17 +185,53 @@ var jLoopChat = function(spec, my) {
   };
 
   return that;
+
+  // PRIVATE FUNCTIONS
+
+  function _checkInitialised() {
+    if (my.initialised === false) {
+      throw new err.JLoopException("jLoopChat not initialised");
+    }
+  }
+
+  function _onAgentMessage(e) {
+    if (_fnOnAgentMessage) {
+      _fnOnAgentMessage(e);
+    }
+  }
+
+  function _onAgentStatusChange(e) {
+    if (_fnOnAgentStatusChange) {
+      _fnOnAgentStatusChange(e);
+    }
+  }
+
+  function _onMessage(m) {
+    var e = JSON.parse(m.data);
+    console.log(e);
+
+    that.transcript.addEvent(e);
+
+    if (e.eventType == "AgentMessage") {
+      _onAgentMessage(e);
+    }
+    else if (e.eventType == "AgentStatusChange") {
+      _onAgentStatusChange(e);
+    }
+    else {
+      throw new err.JLoopException("Unknown event type '" + eventType + "'");
+    }
+  }
 };
 
 module.exports = {
   jLoopChat: jLoopChat,
   model: model,
-  session: session,
   error: err
 };
 
 
-},{"./config":1,"./exceptions":2,"./model":5,"./session":6,"./utils":7}],5:[function(require,module,exports){
+},{"./config":1,"./exceptions":2,"./model":5,"./session":6,"./syncedTranscript":7,"./utils":8}],5:[function(require,module,exports){
 /**
  * Contains constructor functions for objects sent and received
  * to/from the server. Each function takes a spec object, which
@@ -323,6 +318,31 @@ function ServerEndpoint(spec) {
   this.url = spec.url;
 };
 
+function Transcript(spec) {
+  spec = spec || {};
+
+  // List of Event objects
+  this.events = [];
+
+  if (spec.events) {
+    for (var i = 0; i < spec.events.length; ++i) {
+      this.events.push(fromPojo(spec.events[i]));
+    }
+  }
+};
+
+Transcript.prototype.addEvent = function(event) {
+  this.events.push(event);
+};
+
+Transcript.prototype.clear = function() {
+  this.events = [];
+};
+
+Transcript.prototype.isEmpty = function() {
+  return this.events.length === 0;
+};
+
 function fromPojo(pojo) {
   switch (pojo.eventType) {
     case "VisitorMessage":
@@ -352,149 +372,147 @@ module.exports = {
   AgentStatus: AgentStatus,
   AgentList: AgentList,
   ServerEndpoint: ServerEndpoint,
+  Transcript: Transcript,
   fromPojo: fromPojo
 };
 
 
-},{"./exceptions":2,"./utils":7}],6:[function(require,module,exports){
-var model = require("./model");
+},{"./exceptions":2,"./utils":8}],6:[function(require,module,exports){
+function session(uniquePrefix) {
+  var _lifetime = 24 * 60 * 60;
+  var _naming_prefix = uniquePrefix || "jl";
 
-const _NAMING_PREFIX = "jl_";
+  /**
+   * @method setLifetime
+   * @param {Number} seconds
+   */
+  function setLifetime(seconds) {
+    _lifetime = seconds;
+    _put("_expirationDate", (new Date()).getTime() + _lifetime * 1000);
+  }
 
-var _lifetime = 24 * 60 * 60;
-
-function _SessionValue(value) {
-  this.value = value;
-}
-
-function _generateUuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0;
-    var v = c == 'x' ? r : (r & 0x3 | 0x8);
-
-    return v.toString(16);
-  });
-}
-
-function _k(key) {
-  return _NAMING_PREFIX + key;
-}
-
-function _clear() {
-  for (var i = 0; i < localStorage.length; ++i) {
-    var key = localStorage.key(i);
-    if (key.startsWith(_NAMING_PREFIX)) {
-      localStorage.removeItem(key);
-      --i;
+  function put(key, value) {
+    if (hasExpired()) {
+      clear();
     }
-  }
-}
 
-function _get(key, type) {
-  var item = localStorage.getItem(_k(key));
-  if (item === null) {
-    return null;
+    _put(key, value);
   }
 
-  var obj = JSON.parse(item).value;
-  return type != null ? new type(obj) : obj;
-}
-
-function _put(key, value) {
-  localStorage.setItem(_k(key), JSON.stringify(new _SessionValue(value)));
-}
-
-function _remove(key) {
-  localStorage.removeItem(_k(key));
-}
-
-function _createNewSession() { // TODO: Accept name for session
-  _clear();
-
-  _put("visitorId", _generateUuid());
-  _put("visitorName", "");
-  _put("transcript", new Transcript());
-  _put("_expirationDate", (new Date()).getTime() + _lifetime * 1000);
-}
-
-function Transcript(spec) {
-  spec = spec || {};
-
-  // List of model.Event objects
-  this.events = [];
-  this._key = spec.key || "transcript";
-
-  if (spec.events != null) {
-    for (var i = 0; i < spec.events.length; ++i) {
-      this.events.push(model.fromPojo(spec.events[i]));
+  function remove(key) {
+    if (hasExpired()) {
+      clear();
     }
-  }
-};
 
-Transcript.prototype.addEvent = function(event) {
-  this.events.push(event);
-  put(this._key, this);
-};
-
-Transcript.prototype.clear = function() {
-  this.events = [];
-  put(this._key, this);
-};
-
-Transcript.prototype.isEmpty = function() {
-  return this.events.length === 0;
-};
-
-function remove(key) {
-  if (hasExpired()) {
-    _createNewSession();
+    _remove(key);
   }
 
-  _remove(key);
-}
+  function clear() {
+    for (var i = 0; i < localStorage.length; ++i) {
+      var key = localStorage.key(i);
 
-function clear() {
-  _createNewSession();
-}
+      if (key && key.lastIndexOf(_naming_prefix, 0) === 0) {
+        localStorage.removeItem(key);
+        --i;
+      }
+    }
 
-function hasExpired() {
-  var expiration = localStorage.getItem(_k("_expirationDate"));
-  return expiration === null || expiration < (new Date()).getTime();
-}
-
-function get(key, type) {
-  if (hasExpired()) {
-    _createNewSession();
+    _put("_expirationDate", (new Date()).getTime() + _lifetime * 1000);
   }
 
-  return _get(key, type);
-}
-
-function put(key, value) {
-  if (hasExpired()) {
-    _createNewSession();
+  function hasExpired() {
+    var expiration = _get("_expirationDate");
+    return expiration === null || expiration < (new Date()).getTime();
   }
 
-  _put(key, value);
-}
+  function get(key, type) {
+    if (hasExpired()) {
+      clear();
+    }
 
-function setLifetime(seconds) {
-  _lifetime = seconds;
-  _put("_expirationDate", (new Date()).getTime() + _lifetime * 1000);
+    return _get(key, type);
+  }
+
+  return {
+    setLifetime: setLifetime,
+    put: put,
+    remove: remove,
+    clear: clear,
+    hasExpired: hasExpired,
+    get: get
+  };
+
+  // PRIVATE FUNCTIONS
+
+  function _SessionValue(value) {
+    this.value = value;
+  }
+
+  function _k(key) {
+    return _naming_prefix + key;
+  }
+
+  function _put(key, value) {
+    localStorage.setItem(_k(key), JSON.stringify(new _SessionValue(value)));
+  }
+
+  function _get(key, type) {
+    var item = localStorage.getItem(_k(key));
+    if (item === null) {
+      return null;
+    }
+
+    var obj = JSON.parse(item);
+    return type != null ? new type(obj.value) : obj.value;
+  }
+
+  function _remove(key) {
+    localStorage.removeItem(_k(key));
+  }
 }
 
 module.exports = {
-  Transcript: Transcript,
-  setLifetime: setLifetime,
-  hasExpired: hasExpired,
-  remove: remove,
-  get: get,
-  put: put,
-  clear: clear
+  session: session
 };
 
 
-},{"./model":5}],7:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
+var model = require("./model");
+
+function syncedTranscript(session) {
+  var _session = session;
+  var _transcript = _session.get("transcript", model.Transcript) || new model.Transcript();
+
+  function addEvent(e) {
+    _transcript.addEvent(e);
+    _session.put("transcript", _transcript);
+  }
+
+  function clear() {
+    _transcript.clear();
+    _session.put(_transcript);
+  }
+
+  function isEmpty() {
+    return _transcript.isEmpty();
+  }
+
+  function getEvents() {
+    return _transcript.events;
+  }
+
+  return {
+    addEvent: addEvent,
+    clear: clear,
+    isEmpty: isEmpty,
+    getEvents: getEvents
+  };
+}
+
+module.exports = syncedTranscript;
+
+
+},{"./model":5}],8:[function(require,module,exports){
 /**
  * @module utils
  */
@@ -522,11 +540,320 @@ function dictToArray(dict) {
   return res;
 }
 
+function generateUuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0;
+    var v = c == 'x' ? r : (r & 0x3 | 0x8);
+
+    return v.toString(16);
+  });
+}
+
 module.exports = {
   inherits: inherits,
   bind: bind,
-  dictToArray: dictToArray
+  dictToArray: dictToArray,
+  generateUuid: generateUuid
 };
 
 
-},{}]},{},[1,2,3,4,5,6,7]);
+},{}],9:[function(require,module,exports){
+module.exports = function() {
+  return {
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['browserify', 'jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'unit/*.js': ['browserify']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['spec'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  };
+};
+
+},{}],10:[function(require,module,exports){
+var conf = require("./karma-shared.conf.js")();
+
+module.exports = function(config) {
+  conf.files = conf.files.concat([
+    'unit/**.js'
+  ]);
+
+  // level of logging
+  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+  conf.logLevel = config.LOG_INFO;
+
+  config.set(conf);
+};
+
+},{"./karma-shared.conf.js":9}],11:[function(require,module,exports){
+
+},{}],12:[function(require,module,exports){
+var test = test || {};
+
+test.mockWindow = function() {
+  var _x = 0, _y = 0;
+  var _elem = jQuery(window);
+
+  this.scrollLeft = function(x) {
+    if (typeof x === "undefined") {
+      return _x;
+    }
+    else {
+      _x = x;
+    }
+  };
+
+  this.scrollTop = function(y) {
+    if (typeof y === "undefined") {
+      return _y;
+    }
+    else {
+      _y = y;
+    }
+  };
+
+  this.get = function(i) {
+    return _elem.get(i);
+  };
+
+  this.on = function(str, fn) {
+    _elem.on(str, fn);
+  };
+
+  this.triggerHandler = function(str) {
+    _elem.triggerHandler(str);
+  };
+};
+
+},{}],13:[function(require,module,exports){
+"use strict";
+
+var sessions = require("../../js/session");
+var model = require("../../js/model");
+
+describe("session", function() {
+  jasmine.clock().install();
+  var baseTime = new Date(2000, 1, 1);
+
+  beforeEach(function() {
+    jasmine.clock().mockDate(baseTime);
+  });
+
+  it("should clear values", function() {
+    var session = sessions.session();
+    session.put("foo", "bar");
+    session.clear();
+
+    expect(session.get("foo")).toBe(null);
+  });
+
+  it("should store values", function() {
+    var session = sessions.session();
+    session.put("foo", "bar");
+    expect(session.get("foo")).toBe("bar");
+  });
+
+  it("should store complex objects", function() {
+    var session = sessions.session();
+
+    function Thing(spec) {
+      spec = spec || {};
+
+      this.a = spec.a || 0;
+      this.b = spec.b || "";
+      this.c = {
+        d: spec.c.d || 0.0
+      };
+    }
+
+    Thing.prototype.foo = function() {
+      return this.a * this.c.d;
+    };
+
+    var obj = new Thing({
+      a: 2,
+      b: "three",
+      c: {
+        d: 4.1
+      }
+    });
+
+    session.put("obj", obj);
+    var obj2 = session.get("obj", Thing);
+
+    expect(obj2.a).toBe(2);
+    expect(obj2.b).toBe("three");
+    expect(obj2.c.d).toBe(4.1);
+
+    expect(obj2.foo()).toBe(8.2);
+  });
+
+  it("should remove values", function() {
+    var session = sessions.session();
+
+    session.put("foo", "bar");
+    session.remove("foo");
+    expect(session.get("foo")).toBe(null);
+  });
+
+  it("Should expire", function() {
+    var session = sessions.session();
+
+    session.setLifetime(4);
+    session.put("foo", "bar");
+
+    jasmine.clock().tick(4000);;
+    expect(session.get("foo")).toBe("bar");
+
+    jasmine.clock().tick(1);
+    expect(session.get("foo")).toBe(null);
+  });
+
+  it("should contain same data for instances with the same prefix", function() {
+    var session1 = sessions.session("s1");
+    var session2 = sessions.session("s2");
+
+    session1.put("foo", 123);
+    session2.put("foo", 456);
+
+    expect(session1.get("foo")).toBe(123);
+    expect(session2.get("foo")).toBe(456);
+  });
+
+  it("should contain different data for instances with different prefixes", function() {
+    var session1 = sessions.session("s1");
+    var session2 = sessions.session("s1");
+
+    session1.put("foo", 123);
+    session2.put("foo", 456);
+
+    expect(session1.get("foo")).toBe(456);
+    expect(session2.get("foo")).toBe(456);
+  });
+
+  it("should store and restore a Transcript instance", function() {
+    var session = sessions.session();
+
+    var t1 = new model.Transcript();
+    var e1 = new model.VisitorMessage({
+      customerId: "abc",
+      agentId: "def",
+      visitorId: "ace",
+      message: "Hello"
+    });
+
+    t1.addEvent(e1);
+
+    session.put("transcript", t1);
+
+    var t2 = session.get("transcript", model.Transcript);
+    var e2 = t2.events[0];
+
+    expect(e2.customerId).toBe(e1.customerId);
+    expect(e2.agentId).toBe(e1.agentId);
+    expect(e2.visitorId).toBe(e1.visitorId);
+    expect(e2.message).toBe(e1.message);
+  });
+});
+
+
+},{"../../js/model":5,"../../js/session":6}],14:[function(require,module,exports){
+"use strict";
+
+var model = require("../../js/model");
+var sessions = require("../../js/session");
+var syncedTranscript = require("../../js/syncedTranscript");
+
+describe("syncedTranscript", function() {
+  it("should log events", function() {
+    var session = sessions.session();
+    var t1 = syncedTranscript(session);
+
+    var e1 = new model.VisitorMessage({
+      customerId: "abc",
+      agentId: "def",
+      visitorId: "ace",
+      message: "Hello"
+    });
+
+    t1.addEvent(e1);
+
+    var e2 = t1.getEvents()[0];
+
+    expect(e2.customerId).toBe(e1.customerId);
+    expect(e2.agentId).toBe(e1.agentId);
+    expect(e2.visitorId).toBe(e1.visitorId);
+    expect(e2.message).toBe(e1.message);
+  });
+
+  it("should extract transcript from session", function() {
+    var session = sessions.session();
+    var t1 = syncedTranscript(session);
+
+    var e1 = new model.VisitorMessage({
+      customerId: "abc",
+      agentId: "def",
+      visitorId: "ace",
+      message: "Hello"
+    });
+
+    t1.addEvent(e1);
+
+    var t2 = syncedTranscript(session);
+    var e2 = t2.getEvents()[0];
+
+    expect(e2.customerId).toBe(e1.customerId);
+    expect(e2.agentId).toBe(e1.agentId);
+    expect(e2.visitorId).toBe(e1.visitorId);
+    expect(e2.message).toBe(e1.message);
+  });
+});
+
+
+},{"../../js/model":5,"../../js/session":6,"../../js/syncedTranscript":7}]},{},[1,2,3,4,5,6,7,8,9,10,11,12,13,14]);
